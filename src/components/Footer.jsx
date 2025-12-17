960a1d4dc7550c6d8b4575f9d9c0c31b98df81fd
Dhanush1374949 <dhanushkumar0782@gmail.com>
2025-12-17 07:53:39 +0530

    fitts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center fixed bottom-0  w-full bg-green-900 text-white p-1 dark:text-white'>
-        <p className='text-[12px]'>2025 google, Inc.</p>
+        <p className='text-[12px]'>2025 KITA , Inc.</p>
     </div>
   )
 }
